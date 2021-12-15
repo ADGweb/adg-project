4b2e5a3385002f7e1e9f82dfbd90ec994838512f
Дмитрий Авдеев <d.avdeev@reg.ru>
2021-12-15 14:18:24 +0400

    Правит описание rootMargin

--- src/page-blocks/learning-vue-part7/learning-vue-part7.vue
+++ src/page-blocks/learning-vue-part7/learning-vue-part7.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="learning-vue-part7">
-        <p>{{ q }}</p>
+        <div
+            class="learning-vue-part7__textarea-wrapper"
+        >
+            <textarea
+                v-model="value"
+                class="learning-vue-part7__textarea"
+            />
+            {{ value }}
+        </div>
     </div>
 </template>
 
@@ -8,28 +16,28 @@
 export default {
     data() {
         return {
-            q: 'qwqwqwq'
+            value: ''
         }
     }
 }
 </script>
 
-<style lang="scss">
-@import '@/rules/rules';
-
-.learning-vue-part6 {
-    &__input {
-        margin-top: 24px;
-    }
-
-    &__button {
-        padding: 6px 12px;
-        background-color: $green-2;
-
-        &:hover {
-            background-color: $green-1;
-        }
-    }
+<style lang="css">
+.learning-vue-part7__textarea-wrapper {
+    position: relative;
+    width: 300px;
+    min-height: 60px;
+    white-space: pre-line;
 }
 
+.learning-vue-part7__textarea {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    resize: none;
+}
 </style>
